b49186816b822374eed846f4771262a53b5a1d5d
김우재 <woojae@naverz-corp.com>
2023-11-01 14:00:04 +0900

    feat: add change locale button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { appWithTranslation, UserConfig, useTranslation } from "next-i18next";
 import nextI18nConfig from '../next-i18next.config';
 import I18NextHttpBackend from "i18next-http-backend";
 import { PropsWithChildren, Suspense, SuspenseProps, useEffect, useState } from "react";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 /**
  * This component is used to wrap Suspense in order to prevent it from being rendered on the server.
@@ -23,10 +25,16 @@ function SSRSafeSuspense(props: SuspenseProps) {
 
 function Header() {
   const { t } = useTranslation(['common'], {useSuspense: true});
+  const { locale } = useRouter();
 
   return (
     <div>
       {t('hello')}
+      <div>
+        <Link href={''} locale={locale === 'en' ? 'ko' : 'en'}>
+          Change locale
+        </Link>
+      </div>
     </div>
   );
 }
